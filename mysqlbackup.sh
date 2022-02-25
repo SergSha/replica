cb2ece18874c681565b3d80fc95feb174feb0e71
Serg Shagalov <sy.shagalov@yandex.ru>
2022-02-26 01:50:34 +0300

    Modify mysqlbackup.sh and add mysqlrestore.sh

--- mysqlbackup.sh
+++ mysqlbackup.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
+# Create current folder for backup
+date=$(date +'%F_%H-%M-%S')
+mysqlbackuppath="/root/mysqlbackup/$date"
+mkdir $mysqlbackuppath
 MYSQL="/usr/bin/mysql --login-path=backup --skip-column-names"
 MYSQLDUMP="/usr/bin/mysqldump --login-path=backup --single-transaction --source-data=2"
-mysqlbackuppath=/root/mysqlbackup
 
-for db in $($MYSQL -e "SHOW DATABASES LIKE '%_db'");
+for db in $(/$MYSQL -e "SHOW DATABASES LIKE '%db'");
   do
-  mkdir -p /$mysqlbackuppath/$db;
+  mkdir -p $mysqlbackuppath/$db;
   for tbl in $($MYSQL -e "SHOW TABLES FROM $db");
     do
-    $MYSQLDUMP $db $tbl > /$mysqlbackuppath/$db/$tbl.sql;
+    $MYSQLDUMP $db $tbl | /usr/bin/gzip -c > $mysqlbackuppath/$db/$tbl.sql.gz;
     done
-done
+  done
+
+cd /root/mysqlbackup/
+git add -A
+git commit -m "Add mysql backup $date"
+git push origin main
---
+++ mysqlrestore.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Select latest mysql backup directory
+lastmysqlbackup=$(ls -l -t /root/mysqlbackup/ | grep ^d | head -1 | awk '{print $9}')
+echo $lastmysqlbackup
+lastmysqlbackuppath="/root/mysqlbackup/$lastmysqlbackup"
+echo $lastmysqlbackuppath
+
+for db in $(ls -1 $lastmysqlbackuppath);
+  do
+  for tbl in $(ls -1 $lastmysqlbackuppath/$db);
+    do
+    echo "--> $db/$tbl restoring...";
+    zcat $lastmysqlbackuppath/$db/$tbl | /usr/bin/mysql --login-path=backup $db;
+    done
+  done
+
